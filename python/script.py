c9e6234611bc5eb85ebc65c26e55ba19426c19c6
izabela1337 <54004218+izabela1337@users.noreply.github.com>
2021-01-20 20:34:09 +0100

    python + c - WIP

---
+++ python/script.py
@@ -0,0 +1,3 @@
+print('Welecome tho Shitty-Wget-Alternative, this time, Python edition');
+part1 = input("The format of dowloaded files is as follows: \n 'part1' + 'number' + 'extension' \n Please provide part1: ");
+print("part 1 is: " + part1);
