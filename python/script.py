e6c42ab3376338daa2f5222ca87c491a2ad63eac
izabela1337 <54004218+izabela1337@users.noreply.github.com>
2021-01-21 16:14:50 +0100

    reminder to install requests module
    https://via.placeholder.com/151.jpg
    https://docs.python.org/3/installing/index.html

--- python/script.py
+++ python/script.py
@@ -1,3 +1,9 @@
+import requests
+
+r = requests.get(part1, allow_redirects=True)
+
 print('Welecome tho Shitty-Wget-Alternative, this time, Python edition');
 part1 = input("The format of dowloaded files is as follows: \n 'part1' + 'number' + 'extension' \n Please provide part1: ");
 print("part 1 is: " + part1);
+
+open('./files/n.jpg', 'wb').write(r.content)
